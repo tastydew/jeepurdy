7988fa9ac2fc9ba90ee62b1260cc11d5b402e1f2
Fredy Teyssedou <f.teyssedou@gmail.com>
2022-09-29 11:03:26 -0400

    Initial commit of project files

---
+++ Assets/Scripts/LoadQuestionsFromJSON.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[InitializeOnLoad]
+public class LoadQuestionsFromJSON : MonoBehaviour {
+
+	object textFromFile;
+	public LoadQuestionsFromJSON(){
+		Debug.Log("Test");
+	}
+
+	void Start(){
+		
+		textFromFile = Resources.Load<TextAsset>("Assets/JSON/Questions1");
+
+		string convertedText = textFromFile.ToString();
+
+		JeePurdyQuestions questions = JsonUtility.FromJson<JeePurdyQuestions>(convertedText);
+
+	}
+
+
+	
+
+	
+}
+
+[System.Serializable]
+public class JeePurdyQuestions{
+
+	public class Categories{
+		public string CatOne;
+		public string CatTwo;
+		public string CatThree;
+		public string CatFour;
+		public string CatFive;
+		public string CatSix;
+		public string CatSeven;
+		public string CatEight;
+	}
+
+	public class Answers{
+
+		public string CategoryID;
+		public int AnswerValue;
+		public string AnswerText;
+	}
+
+}
